7d0df20ef866e83b2689187a7dd3892bff2dd332
NotEvenARobert <132989142+NotEvenARobert@users.noreply.github.com>
2025-10-02 02:49:23 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+
+    int data;
+    struct node *next;
+
+};
+
+struct node* insert(struct node* list, int intData);
+struct node* del(struct node* list, int intData);
+void print(struct node *list);
+void freeList(struct node* list);
+void copy(struct node *q, struct node **s);
+
+
+int main(void) {
+  
+    int number = 0, choice = 0;
+    struct node *pList = NULL;
+    struct node *nList = NULL;
+
+    while(choice != 4) {
+
+        printf("\nDo you want to (1) insert, (2) delete, (3) Copy (4) quit.\n");
+        scanf("%d", &choice);
+        printf("Your choice is %d\n", choice);
+
+        if(choice == 1) {
+
+            printf("Enter the value to insert\n");
+            scanf("%d", &number);
+            pList = insert(pList, number);
+            // Look at the list.
+            printf("Items in linked list: ");
+            print(pList);
+            printf("\n");
+          
+        } else if(choice == 2) {
+
+            printf("Enter the value to delete.\n");
+            scanf("%d", &number);
+            pList = del(pList, number);
+
+            printf("Items in linked list: ");
+            print(pList);
+            printf("\n");
+          
+        } else if(choice == 3) {
+          
+            if(nList) {
+              
+                freeList(nList);
+              
+            }  
+            copy(pList, &nList); 
+
+            printf("Items in NEW linked list: ");
+            print(nList);
+            printf("\n");
+          
+        } else {
+          
+            break;
+          
+        }
+      
+    }
+
+    freeList(nList);
+    freeList(pList);
+    printf("\nBye..\n");
+    return 0;
+  
+}
+
+void print(struct node *list) {
+
+    if(list == NULL) {
+      
+        return;
+      
+    }
+    printf("->%d", list->data);
+    print(list->next);
+  
+}
+
+struct node* insert(struct node* list, int intData) {
+
+    if(list == NULL || intData < list->data) {
+
+        struct node* newPtr = (struct node*)malloc(sizeof(struct node));
+        newPtr->data = intData;
+        newPtr->next = list;
+        return newPtr;
+      
+    }
+
+    list->next = insert(list->next, intData);
+    return list;
+  
+}
